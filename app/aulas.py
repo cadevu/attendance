5493240bdc5189e5a9a80b8d718a006800ad24d2
cadevu <carvalhocarlos233@gmail.com>
2023-05-23 00:09:29 -0300

    initial commit

---
+++ app/aulas.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+from . import db
+from .models import Aula
+import random,string
+aulas = Blueprint('aulas',__name__)
+
+
+@aulas.route('/nova_aula')
+@login_required
+def nova_aula():
+    return render_template("aula.html")
+
+@aulas.route('/nova_aula',methods = ['POST'])
+@login_required
+def nova_aula_post():
+    nome_disciplina = request.form.get('name')
+    cod_disciplina = request.form.get('codigo')
+    turma = request.form.get('turma')
+    data_aula = request.form.get('data_aula')
+    cod_auth = (''.join(random.choices(string.ascii_letters,k=5)))
+
+    aula = Aula(nome_disciplina = nome_disciplina,
+                cod_disciplina = cod_disciplina,
+                turma = turma,
+                data_aula = data_aula,
+                professor = current_user.matricula,
+                cod_auth = cod_auth,
+                alunos_presentes = [],
+                status = 'OPEN')
+    db.session.add(aula)
+    db.session.commit()
+    return redirect(url_for('aulas.codigo_aula',codigo = aula.cod_auth))
+
+@aulas.route('/presenca')
+def presenca():
+    return render_template('presenca.html')
+
+@aulas.route('/codigo_aula/<codigo>',methods = ['POST','GET'])
+def codigo_aula(codigo):
+    if request.method == 'POST':
+        aula = Aula.query.filter_by(cod_auth = codigo).first()
+        aula.status = 'CLOSED'
+        db.session.commit()
+        return "<p> Chamada fechada<p>"
+    else:
+        return render_template('codigo_aula.html',codigo = codigo)
+
+@aulas.route('/presenca',methods = ['POST'])
+def presenca_post():
+    cod_aula = request.form.get('cod_aula')
+    matricula_aluno = request.form.get('matricula')
+    nome_aluno = request.form.get('nome')
+    aula = Aula.query.filter_by(cod_auth=cod_aula).first()
+    if not aula:
+        flash("Aula não cadastrada no sistema",'error')
+        return redirect(url_for('aulas.presenca'))
+    if aula.status == 'CLOSED':
+        flash("Chamada já encerrada",'error')
+        return redirect(url_for('aulas.presenca'))
+    print(aula.alunos_presentes)
+    aula.alunos_presentes.append([nome_aluno, matricula_aluno])
+    db.session.commit()
+    flash('Presença registrada com sucesso!','info')
+    return render_template('presenca.html',nome = nome_aluno, matricula = matricula_aluno, data = aula.data_aula,
+                           turma = aula.turma, disc = aula.nome_disciplina)
+
+@aulas.route('/turmas')
+@login_required
+def turmas():
+    matricula_prof = current_user.matricula
+    turmas = Aula.query.filter_by(professor = matricula_prof).all()
+    turmas_json =[]
+    for i in turmas:
+        turmas_json.append(i.to_dict())
+
+    return render_template('turmas.html',turmas = turmas_json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/auth.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from flask import render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Professor
+from flask_login import login_user, login_required, logout_user
+
+auth = Blueprint('auth',__name__)
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+@auth.route('/login', methods = ['POST'])
+def login_post():
+    matricula = request.form.get('matricula')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    prof = Professor.query.filter_by(matricula = matricula).first()
+
+
+    if not prof:
+        flash("Professor não cadastrado na base de dados")
+        return redirect(url_for('auth.login'))
+    if not check_password_hash(prof.password, password):
+        print('wrong psw')
+        flash("Senha incorreta, tente novamente.")
+
+        return redirect(url_for('auth.login'))
+    login_user(prof, remember = remember)
+    return redirect(url_for('main.profile'))
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup',methods = ['POST'])
+def signup_post():
+    email = request.form.get('email')
+    matricula = request.form.get('matricula')
+    nome = request.form.get('nome')
+    password = request.form.get("password")
+    # if len(matricula) > 9:
+    #     flash("Matricula no formato inválido")
+    #     return redirect(url_for('auth.signup'))
+
+    prof = Professor.query.filter_by(matricula = matricula).first()
+    if prof:
+      flash('matricula já cadastrada.','error')
+      return redirect(url_for('auth.login'))
+    print(prof)
+    new_prof = Professor(email = email,
+                        matricula = matricula,
+                        nome = nome,
+                        password =  generate_password_hash(password,method='sha256'))
+    db.session.add(new_prof)
+    db.session.commit()
+
+    return redirect(url_for('main.profile'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
